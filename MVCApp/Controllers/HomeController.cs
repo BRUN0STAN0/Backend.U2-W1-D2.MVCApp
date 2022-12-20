54b16cdb2e85b9c7c51da5123274b8087bb3dc6b
RobDab <roberto.dabb@gmail.com>
2022-12-20 15:22:21 +0100

    Aggiungere i file di progetto.

---
+++ MVCApp/Controllers/HomeController.cs
@@ -0,0 +1,184 @@
+﻿using MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["ConToDipendentiDB"].ToString();
+            con.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "SELECT * FROM DipendenteTab";
+            command.Connection = con;
+
+            SqlDataReader reader = command.ExecuteReader();
+            List<Dipendente> EmployeeList = new List<Dipendente>();
+
+            if (reader.HasRows)
+            {
+
+                while (reader.Read())
+                {
+                    Dipendente employee = new Dipendente();
+                    employee.IDDipendente = Convert.ToInt32(reader["IDDipendente"]);
+                    employee.Nome = reader["Nome"].ToString();
+                    employee.Cognome = reader["Cognome"].ToString();
+                    employee.Stipendio = Convert.ToDouble(reader["Stipendio"]);
+                    employee.Coniugato = Convert.ToBoolean(reader["Coniugato"]);
+                    EmployeeList.Add(employee);
+                }
+            }
+
+            con.Close();
+
+            
+            return View(EmployeeList);
+        }
+
+        // Aggiunta dipendente
+
+        
+        public ActionResult CreateEmployee() 
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateEmployee(Dipendente employee)
+        {
+            SqlConnection con = new SqlConnection();
+            try
+            {
+                
+                con.ConnectionString = ConfigurationManager.ConnectionStrings["ConToDipendentiDB"].ToString();
+                con.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Parameters.AddWithValue("@Nome", employee.Nome);
+                command.Parameters.AddWithValue("@Cognome", employee.Cognome);
+                command.Parameters.AddWithValue("@Stipendio", employee.Stipendio);
+                command.Parameters.AddWithValue("@Coniugato", employee.Coniugato);
+
+                command.CommandText = "INSERT INTO DipendenteTab VALUES (@Nome , @Cognome , @Stipendio, @Coniugato)";
+                command.Connection = con;
+
+
+                command.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+
+                con.Close();
+            }
+
+            con.Close();
+            return RedirectToAction("Index");
+        }
+
+        // Update dipendente
+        public ActionResult EditEmployee(int id) 
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["ConToDipendentiDB"].ToString();
+            con.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Parameters.AddWithValue("@ID", id);
+
+            command.CommandText = "SELECT * FROM DipendenteTab WHERE IDDipendente = @ID";
+            command.Connection = con;
+
+            SqlDataReader reader = command.ExecuteReader();
+            Dipendente employee = new Dipendente();
+            if (reader.HasRows)
+            {
+                while(reader.Read())
+                {
+                    employee.IDDipendente = Convert.ToInt32(reader["IDDipendente"]);
+                    employee.Nome = reader["Nome"].ToString();
+                    employee.Cognome = reader["Cognome"].ToString();
+                    employee.Stipendio = Convert.ToDouble(reader["Stipendio"]);
+                    employee.Coniugato = Convert.ToBoolean(reader["Coniugato"]);
+
+                }
+            }
+
+            con.Close();
+            return View(employee); 
+        }
+
+        [HttpPost]
+        public ActionResult EditEmployee(Dipendente employee)
+        {
+            SqlConnection con = new SqlConnection();
+            try { 
+            
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["ConToDipendentiDB"].ToString();
+            con.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Parameters.AddWithValue("@ID", employee.IDDipendente);
+            command.Parameters.AddWithValue("@Nome", employee.Nome);
+            command.Parameters.AddWithValue("@Cognome", employee.Cognome);
+            command.Parameters.AddWithValue("@Stipendio", employee.Stipendio);
+            command.Parameters.AddWithValue("@Coniugato", employee.Coniugato);
+
+            command.CommandText = "UPDATE DipendenteTab SET Nome = @Nome , Cognome = @Cognome , Stipendio = @Stipendio , Coniugato = @Coniugato WHERE IDDipendente = @ID";
+            command.Connection = con;
+
+            command.ExecuteNonQuery();
+
+
+            }catch(Exception ex)
+            {
+                con.Close();
+            }
+
+            con.Close();
+
+            return RedirectToAction("Index");
+        }
+
+        // Eliminazione dipendente 
+
+        public ActionResult DeleteEmployee(int id)
+        {
+            SqlConnection con = new SqlConnection();
+            try
+            {
+                con.ConnectionString = ConfigurationManager.ConnectionStrings["ConToDipendentiDB"].ToString();
+                con.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Parameters.AddWithValue("@ID", id);
+
+                command.CommandText = "DELETE FROM DipendenteTab WHERE IDDipendente = @ID";
+                command.Connection= con;
+
+                command.ExecuteNonQuery();
+
+            }
+            catch(Exception ex) 
+            {
+                con.Close();
+            }
+
+            con.Close();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ MVCApp/Models/Dipendente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class Dipendente
+    {
+        public int IDDipendente { get; set; }
+
+
+        [Display(Name = "Nome Dipendente")]
+        [Required(ErrorMessage= "Nome Obbligatorio")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Cognome Dipendente")]
+        [Required(ErrorMessage = "Cognome Obbligatorio")]
+        public string Cognome { get; set; }
+
+        [Display(Name = "Stipendio Dipendente in €")]
+        [Required(ErrorMessage = "Droppa sto stipendio")]
+        public double Stipendio { get; set; }
+
+        
+        public bool Coniugato { get; set;  }
+    }
+}
